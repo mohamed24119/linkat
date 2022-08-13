44029527174327efd3133045bf00956ebaba44aa
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2022-08-13 18:19:41 +0200

    Create news.js
    دليل المواقع للصحف والمجلات وروابط الحسابات على مواقع التواصل الاجتماعي

---
+++ news.js
@@ -0,0 +1,172 @@
+const objNews = [
+    {
+        "objAddTitle": "اليوم السابع",
+        "objAddUrl": "https://www.youm7.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "youm7",
+        "objTwitter": "youm7",
+        "objYoutube": "user/mubasheryoum7",
+        "objInstagram": "youm7",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "جريدة الدستور",
+        "objAddUrl": "https://www.dostor.org",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "ElDostorEgypt",
+        "objTwitter": "ElDostorEgypt",
+        "objYoutube": "user/aldostornews",
+        "objInstagram": "eldostoregypt",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "مصراوي",
+        "objAddUrl": "http://www.masrawy.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "Masrawy",
+        "objTwitter": "masrawy",
+        "objYoutube": "channel/UCpfZqY4SGVhHxF8x4XMWNew",
+        "objInstagram": "Masrawy",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "جريدة المصري اليوم",
+        "objAddUrl": "https://www.almasryalyoum.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "almasryalyoum",
+        "objTwitter": "AlMasryAlYoum",
+        "objYoutube": "",
+        "objInstagram": "almasryalyoum",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "صدى البلد",
+        "objAddUrl": "https://www.elbalad.news",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "ElBaladOfficial",
+        "objTwitter": "ElBaladOfficial",
+        "objYoutube": "c/ElBaladOfficial",
+        "objInstagram": "ElBaladOfficial",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "بوابة الشروق",
+        "objAddUrl": "https://www.shorouknews.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "shorouknews",
+        "objTwitter": "shorouk_news",
+        "objYoutube": "channel/UCGONWo6kCXGwtyA8SHrHIAw",
+        "objInstagram": "shorouknews",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "جريدة الوطن",
+        "objAddUrl": "https://www.elwatannews.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "ElWatanNews",
+        "objTwitter": "elwatannews",
+        "objYoutube": "user/ElWatanNews",
+        "objInstagram": "elwatannews",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "بوابة أخبار اليوم",
+        "objAddUrl": "https://akhbarelyom.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "AkhbarElYom",
+        "objTwitter": "akhbarelyom",
+        "objYoutube": "user/akhbarelyoumtv",
+        "objInstagram": "akhbarelyom",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "جريدة فيتو",
+        "objAddUrl": "https://www.vetogate.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "Vetogate",
+        "objTwitter": "Vetogate",
+        "objYoutube": "user/VetogateOfficial",
+        "objInstagram": "vetogate",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "البوابة نيوز",
+        "objAddUrl": "https://www.albawabhnews.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "Albawabanewspaper",
+        "objTwitter": "AlBawabaNews",
+        "objYoutube": "channel/UCU0s_Dr2oPn6TGz25cj205w",
+        "objInstagram": "albawaba_eg",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "بوابة الأهرام",
+        "objAddUrl": "https://ahram.org.eg",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "alahram",
+        "objTwitter": "AlAhram",
+        "objYoutube": "channel/UCIqfXUG5dbM8iGINnqsdriQ",
+        "objInstagram": "alahram",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "أخبار مصر",
+        "objAddUrl": "https://www.maspero.eg/wps/portal/home/egynews",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "egynews.official",
+        "objTwitter": "",
+        "objYoutube": "c/EgyptNewsCenter",
+        "objInstagram": "",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "بوابة الفجر",
+        "objAddUrl": "https://www.elfagr.org",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "ElfagrNewsEg",
+        "objTwitter": "ElfagrN",
+        "objYoutube": "c/ElFagrNewsTV",
+        "objInstagram": "elfagrnews",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "بوابة الأسبوع",
+        "objAddUrl": "https://www.elaosboa.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "ElAosboaNews",
+        "objTwitter": "elaosboa2021",
+        "objYoutube": "channel/UCgFc9Lt_MPmSXLC9Qi6b1OA",
+        "objInstagram": "elaosboa",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "جريدة المساء",
+        "objAddUrl": "https://almessa.gomhuriaonline.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "AlmessaNewspaper",
+        "objTwitter": "",
+        "objYoutube": "",
+        "objInstagram": "",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "الجمهورية أون لاين",
+        "objAddUrl": "https://www.gomhuriaonline.com",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "algomhuriaonline",
+        "objTwitter": "agomhuriaonline",
+        "objYoutube": "",
+        "objInstagram": "",
+        "objLinkedin": ""
+    },
+    {
+        "objAddTitle": "جريدة النبأ",
+        "objAddUrl": "https://www.elnabaa.net",
+        "objCategory": "صحف ومجلات",
+        "objFacebook": "alnabaalwatany",
+        "objTwitter": "alnabaa_news",
+        "objYoutube": "channel/UCzTcKJYL_v78UTGvcdj2A2Q",
+        "objInstagram": "",
+        "objLinkedin": ""
+    }
+];
